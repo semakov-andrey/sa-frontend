4f5d831c4a9028504ac207c599683ec37a17c5c8
Semakov Andrey <semakov.andrey.web@yandex.ru>
2023-05-27 04:39:44 +0600

    Added types package

---
+++ packages/types/global.d.ts
@@ -0,0 +1,58 @@
+type FunctionType<T = unknown, R = unknown> = (...args: Array<T>) => R;
+
+type ObjectKey = string | number | symbol;
+
+type ObjectType<T = unknown> = Record<ObjectKey, T>;
+
+type ObjectDefType<K extends ObjectKey, T = unknown> = Record<K, T>;
+
+type Nullable<T> = T | null;
+
+type Optional<T> = T | undefined;
+
+type Absent<T> = T | null | undefined;
+
+type OneOrMore<T> = T | Array<T>;
+
+type ArrayInnerType<T> = T extends Array<infer R> ? R : never;
+
+type KeyOf<T> = keyof T;
+
+type ValueOf<T> = T[KeyOf<T>];
+
+type EntryOf<T> = { [K in KeyOf<T>]: [K, T[K]] }[KeyOf<T>];
+
+interface ObjectConstructor {
+  keys<T>(o: T): T extends Array<unknown> ? string : Array<KeyOf<T>>;
+
+  values<T>(o: T): T extends Array<infer R> ? R : Array<T[KeyOf<T>]>;
+
+  entries<T>(o: T): T extends Array<infer R> ? Array<[ string, R ]> : Array<EntryOf<T>>;
+
+  fromEntries<T>(entries: Array<EntryOf<T>>): T;
+}
+
+interface Array<T> {
+  includes<U>(searchElement: U, fromIndex?: number): U extends T ? boolean : false;
+}
+
+type Modify<T, R> = Omit<T, KeyOf<R>> & R;
+
+type PromiseResolve<T> = (value: T) => void;
+
+type PromiseReject<T> = (reason?: T) => void;
+
+type UnionToIntersection<U> =
+  (U extends unknown ? (k: U) => void : never) extends ((k: infer I) => void) ? I : never;
+
+type OmitX<T, K> = T extends T
+  ? T extends K
+    ? never
+    : T
+  : never;
+
+type UnionToTuple<T, K = T> = [T] extends [never]
+  ? []
+  : T extends T
+    ? [T, ...UnionToTuple<OmitX<K, T>>]
+    : [];
