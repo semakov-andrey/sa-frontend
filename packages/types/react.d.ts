5113e714416c04e9f17c01be9d341f88c263b8c5
Semakov Andrey <semakov.andrey.web@yandex.ru>
2023-06-02 04:51:07 +0600

    Added react icon props

--- packages/types/react.d.ts
+++ packages/types/react.d.ts
@@ -1,3 +1,8 @@
 type TEntireElement = OneOrMore<JSX.Element | string | number | null | undefined>;
 
 type TExistElement = OneOrMore<JSX.Element | string | number>;
+
+interface IIconProps {
+  className?: string;
+  style?: React.CSSProperties;
+}
