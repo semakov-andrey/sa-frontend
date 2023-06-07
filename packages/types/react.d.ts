c3cff2f00864161f7d4b5ef9e32b7fabfe7416fe
Semakov Andrey <semakov.andrey.web@yandex.ru>
2023-06-07 13:02:59 +0600

    Added react custom properties

--- packages/types/react.d.ts
+++ packages/types/react.d.ts
@@ -1,3 +1,12 @@
+import 'react';
+import CSS from 'csstype';
+
+declare module 'react' {
+  interface CSSProperties extends CSS.Properties<string | number> {
+    [key: `--${ string }`]: Optional<string>;
+  }
+}
+
 type TEntireElement = OneOrMore<JSX.Element | string | number | null | undefined>;
 
 type TExistElement = OneOrMore<JSX.Element | string | number>;
