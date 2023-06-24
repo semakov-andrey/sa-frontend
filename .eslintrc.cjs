c923a217c7a6d2b297954b6d1d441a5230f7ddc9
Semakov Andrey <semakov.andrey.web@yandex.ru>
2023-06-25 04:31:16 +0600

    Changed urls

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   extends: [
-    './packages/eslint/configs/main.config.cjs',
-    './packages/eslint/configs/progress.config.cjs'
+    './packages/linting/configs/main.config.cjs',
+    './packages/linting/configs/progress.config.cjs'
   ]
 };
