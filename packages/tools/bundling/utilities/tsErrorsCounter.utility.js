8f9b30c5454e3454db4b42a58be94906366a6239
Semakov Andrey <semakov.andrey.web@yandex.ru>
2023-06-28 06:01:54 +0600

    Added bundling package

---
+++ packages/tools/bundling/utilities/tsErrorsCounter.utility.js
@@ -0,0 +1,11 @@
+export const TSErrorsCounter = (forkTsCheckerWebpackPlugin) =>
+  class TSErrorsCounterPlugin {
+    apply(compiler) {
+      const waitTime = 1500;
+      forkTsCheckerWebpackPlugin.getCompilerHooks(compiler).issues.tap('MyPlugin', (issues) => {
+        if (issues.length > 0) {
+          setTimeout(() => console.info(`${ issues.length } typescript errors`), waitTime);
+        }
+      });
+    }
+  };
