450359d9a5130573a047a8424b5f5d381c5c4044
Semakov Andrey <semakov.andrey.web@yandex.ru>
2023-08-22 05:13:41 +0600

    use event hook

---
+++ packages/source/presentation/common/hooks/useEvent.hook.ts
@@ -0,0 +1,16 @@
+import { useRef, useLayoutEffect } from 'react';
+
+export const useEvent = <T extends (...args: Array<never>) => unknown>(fn: T): T => {
+  const ref = useRef<[T, T]>(
+    [
+      fn,
+      ((...args: Parameters<T>): ReturnType<T> => ref[0](...args) as ReturnType<T>) as T
+    ]
+  ).current;
+
+  useLayoutEffect(() => {
+    ref[0] = fn;
+  });
+
+  return ref[1];
+};
--- tmp/useEvent.ts
+++
@@ -1,7 +0,0 @@
-export const useEvent = (fn) => {
-  const ref = useRef([fn, (...args) => ref[0](...args)]).current;
-  useLayoutEffect(() => {
-    ref[0] = fn;
-  });
-  return ref[1];
-}
