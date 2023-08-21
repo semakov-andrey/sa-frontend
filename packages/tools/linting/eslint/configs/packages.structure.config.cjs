5734446c837ec5f258774e2e702e8e871561abce
Semakov Andrey <semakov.andrey.web@yandex.ru>
2023-08-22 04:47:54 +0600

    Updated structure

--- packages/tools/linting/eslint/configs/packages.structure.config.cjs
+++ packages/tools/linting/eslint/configs/packages.structure.config.cjs
@@ -1,3 +1,142 @@
+const REACT_APP_COMMON_STRUCTURE = {
+  assets: {
+    file1: '(.*).asset.svg.d.ts',
+    file2: '(.*).asset.png.d.ts'
+  },
+  constants: {
+    file: '(.*).constants?.ts'
+  },
+  contexts: {
+    '(.*).context': {
+      file1: '\\1.constants?.ts',
+      file2: '\\1.context.ts',
+      file3: '\\1.types?.ts'
+    },
+    'file': '(.*).context.ts'
+  },
+  hooks: {
+    'use(.*).hook': {
+      file1: 'use\\1.constants?.ts',
+      file2: 'use\\1.hook.ts',
+      file3: 'use\\1.types?.ts'
+    },
+    'file': 'use(.*).hook.ts'
+  },
+  stores: {
+    '(.*).store': {
+      file1: '\\1.constants?.ts',
+      file2: '\\1.store.ts',
+      file3: '\\1.types?.ts'
+    },
+    'file': '(.*).store.ts'
+  },
+  stories: {
+    file: '(.*).stor(y|ies).ts'
+  },
+  styles: {
+    file: '(.*).styles?.css.d.ts'
+  },
+  tests: {
+    file: '(.*).test.tsx?'
+  },
+  types: {
+    file: '(.*).types?(.d)?.ts'
+  },
+  utilities: {
+    file: '(.*).utilit(y|ies).ts'
+  }
+};
+
+const REACT_APP_INNER_STRUCTURE = JSON.parse(
+  JSON.stringify(
+    Object.fromEntries(
+      Object.entries(REACT_APP_COMMON_STRUCTURE).map(([ key, value ]) => [ `\\4.${ key }`, value ])
+    )
+  ).replaceAll('\\1', '\\5')
+);
+
+const STRUCTURE = {
+  src: {
+    domain: {
+      '(entities|contracts)': {
+        '(.*)': {
+          constants: {
+            file: '(.*).constants?.ts'
+          },
+          utilities: {
+            file: '(.*).utilit(y|ies).ts'
+          },
+          file: '\\1.ts'
+        }
+      }
+    },
+    application: {
+      '(.*)': {
+        file: '\\1.ts'
+      },
+      'utilities': {
+        file: '(.*).utilit(y|ies).ts'
+      },
+      'contracts': {
+        '(.*)': {
+          '\\1.constants': {
+            file: '(.*).constants?.ts'
+          },
+          'file1': '\\1.constants?.ts',
+          'file2': '\\1.ts'
+        }
+      },
+      'types': {
+        file: '(.*).types?(.d)?.ts'
+      }
+    },
+    presentation: {
+      'common': REACT_APP_COMMON_STRUCTURE,
+      '(main|pages|segments|uiKit)(/(.*).components)?': {
+        '(.*)': REACT_APP_INNER_STRUCTURE,
+        'file1': '\\4.asset.svg.d.ts',
+        'file2': '\\4.asset.png.d.ts',
+        'file3': '\\4.constants?.ts',
+        'file4': '\\4.contexts?.ts',
+        'file5': '\\4.hooks?.ts',
+        'file6': '\\4.stor(y|ies).tsx?',
+        'file7': '\\4.styles?.css.d.ts',
+        'file8': '\\4.tests?.tsx?',
+        'file9': '\\4.utilit(y|ies).ts',
+        'file10': '\\4.tsx?'
+      },
+      'file1': 'index.html',
+      'file2': 'index.tsx'
+    },
+    infrastructure: {
+      '(.*)': {
+        '\\1.(schemas)': {
+          file: '(.*).schemas?.ts'
+        },
+        'file1': '\\1.(utilities).ts',
+        'file2': '\\1.ts'
+      },
+      'file': 'index.ts'
+    },
+    file: 'index.ts'
+  }
+};
+
+const processDir = (obj, prefix) =>
+  Object.entries(obj).map(([ key, value ]) => {
+    const nextPrefix = [ ...prefix ? [ prefix ] : [], key ].join('/');
+
+    if (typeof value === 'string') {
+      return [ [ ...prefix ? [ prefix ] : [], value ].join('/') ];
+    }
+
+    if (typeof value === 'object') {
+      return processDir(value, nextPrefix);
+    }
+
+    return [];
+  }).flat();
+
 module.exports = {
   overrides: [
     {
@@ -7,50 +146,7 @@ module.exports = {
       },
       rules: {
         'rulesdir/structure': [ 'error', {
-          rules: [
-            'src/domain/entities/(.*)/\\1.ts',
-            'src/domain/entities/(.*)/\\1.constants/(.*).constant.ts',
-            'src/domain/entities/(.*)/\\1.utilities/(.*).utility.ts',
-            'src/domain/entities/(.*)/\\1.(constants|utilities).ts',
-            'src/domain/contracts/(.*)/\\1.constants/(.*).constant.ts',
-            'src/domain/contracts/(.*)/\\1.utilities/(.*).utility.ts',
-            'src/domain/contracts/(.*)/\\1.(constants|utilities).ts',
-            'src/domain/contracts/(.*)/\\1.ts',
-            'src/application/(.*)/\\1.ts',
-            'src/application/utilities/(.*).utility.ts',
-            'src/application/contracts/(.*)/\\1.constants/(.*).constant.ts',
-            'src/application/contracts/(.*)/\\1.constants.ts',
-            'src/application/contracts/(.*)/\\1.ts',
-            'src/application/types/(.*).type(.d)?.ts',
-            'src/presentation/common/assets/(.*).asset.svg.d.ts',
-            'src/presentation/common/constants/(.*).constant.ts',
-            'src/presentation/common/contexts/(.*)(.context/\\1)?.(constant|context|type).ts',
-            'src/presentation/common/hooks/use(.*)(.hook/use\\1)?.(constant|hook|type).ts',
-            'src/presentation/common/stores/(.*)(.store/\\1)?.(constant|store|type).ts',
-            'src/presentation/common/stories/(.*).story.ts',
-            'src/presentation/common/styles/(.*).styles.css.d.ts',
-            'src/presentation/common/tests/(.*).test.tsx?',
-            'src/presentation/common/types/(.*).type(.d)?.ts',
-            'src/presentation/common/utilities/(.*).utility.ts',
-            'src/presentation/(main|pages|segments|uiKit)(/(.*).components)?/(.*)/\\4.assets/(.*).asset.svg.d.ts',
-            'src/presentation/(main|pages|segments|uiKit)(/(.*).components)?/(.*)/\\4.constants/(.*).constant.ts',
-            'src/presentation/(main|pages|segments|uiKit)(/(.*).components)?/(.*)/\\4.contexts/(.*)(.context/\\5)?.(constant|context|type).ts',
-            'src/presentation/(main|pages|segments|uiKit)(/(.*).components)?/(.*)/\\4.hooks/use(.*)(.hook/use\\5)?.(constant|hook|type).ts',
-            'src/presentation/(main|pages|segments|uiKit)(/(.*).components)?/(.*)/\\4.stores/(.*)(.store/\\5)?.(constant|store|type).ts',
-            'src/presentation/(main|pages|segments|uiKit)(/(.*).components)?/(.*)/\\4.stories/(.*).story.ts',
-            'src/presentation/(main|pages|segments|uiKit)(/(.*).components)?/(.*)/\\4.styles/(.*).styles.css.d.ts',
-            'src/presentation/(main|pages|segments|uiKit)(/(.*).components)?/(.*)/\\4.tests/(.*).test.tsx?',
-            'src/presentation/(main|pages|segments|uiKit)(/(.*).components)?/(.*)/\\4.types/(.*).type.ts',
-            'src/presentation/(main|pages|segments|uiKit)(/(.*).components)?/(.*)/\\4.utilities/(.*).utility.ts',
-            'src/presentation/(main|pages|segments|uiKit)(/(.*).components)?/(.*)/\\4(.(assets.svg.d|constants|contexts|hooks|stores|stories|styles.css.d|tests|types|utilities))?.tsx?',
-            'src/presentation/index.html',
-            'src/presentation/index.tsx',
-            'src/infrastructure/(.*)/\\1.(schemas)/(.*).(schema).ts',
-            'src/infrastructure/(.*)/\\1.(utilities).ts',
-            'src/infrastructure/(.*)/\\1.ts',
-            'src/infrastructure/index.ts',
-            'src/index.ts'
-          ]
+          rules: processDir(STRUCTURE)
         } ]
       }
     }
