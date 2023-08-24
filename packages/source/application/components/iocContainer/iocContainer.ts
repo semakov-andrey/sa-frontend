4d273f6f14bc6de644f0872f10cf6adc00001827
Semakov Andrey <semakov.andrey.web@yandex.ru>
2023-08-25 04:25:10 +0600

    Patched ioc container

--- packages/source/application/components/iocContainer/iocContainer.ts
+++ packages/source/application/components/iocContainer/iocContainer.ts
@@ -3,11 +3,10 @@ import { isset } from '../../utilities/typeGuards.utilities';
 class IoCContainer {
   private instances: Map<symbol, unknown> = new Map();
 
-  public get = <T>(token: symbol): T | undefined => {
+  public get = <T>(token: symbol): T | never => {
     const constructor = this.instances.get(token);
     if (!isset(constructor)) {
-      console.error(`DI failed: token "${ token.description ?? 'NoName token' }" not found`);
-      return undefined;
+      throw new Error(`DI failed: token "${ token.description ?? 'NoName token' }" not found`);
     }
 
     return constructor as T;
--- packages/source/presentation/main/IoCContainer/IoCContainer.hook.tsx
+++ packages/source/presentation/main/IoCContainer/IoCContainer.hook.tsx
@@ -2,5 +2,5 @@ import { useContext } from 'react';
 
 import { IoCContainerContext } from './IoCContainer.context';
 
-export const useInject = <T extends unknown>(token: symbol): T | undefined =>
+export const useInject = <T extends unknown>(token: symbol): T | never =>
   useContext(IoCContainerContext).get(token);
