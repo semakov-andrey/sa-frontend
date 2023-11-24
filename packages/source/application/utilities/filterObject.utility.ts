368b38dff40a73f703ac1858daa923461ea8c43e
Semakov Andrey <asemakov@cinimex.ru>
2023-11-24 19:12:52 +0600

    commit

--- packages/source/application/utilities/filterObject.utility.ts
+++ packages/source/application/utilities/filterObject.utility.ts
@@ -1,4 +1,4 @@
-export const filterObject = <T extends object>(o: T, keys: Array<string>): T => {
+export const filterObject = <T extends object, D extends Array<keyof T>>(o: T, keys: D): Pick<T, D[number]> => {
   const entries: Array<EntryOf<T>> = Object.entries(o).filter((entry: EntryOf<T>) => keys.includes(entry[0]));
   return Object.fromEntries(entries);
 };
