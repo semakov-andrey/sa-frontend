fe45115a9c92f414eab330df88db591962f5e0be
Semakov Andrey <asemakov@cinimex.ru>
2024-01-08 02:28:48 +0600

    c

--- packages/source/application/utilities/filterObject.utility.ts
+++ packages/source/application/utilities/filterObject.utility.ts
@@ -1,4 +1,9 @@
-export const filterObject = <T extends object, D extends (keyof T)[]>(o: T, keys: D): Pick<T, D[number]> => {
-  const entries: EntryOf<T>[] = Object.entries(o).filter((entry: EntryOf<T>) => keys.includes(entry[0]));
+export const filterObjectByKeys = <T extends object, D extends (keyof T)[]>(o: T, allowedKeys: D): Pick<T, D[number]> => {
+  const entries: EntryOf<T>[] = Object.entries(o).filter((entry: EntryOf<T>) => allowedKeys.includes(entry[0]));
+  return Object.fromEntries(entries);
+};
+
+export const filterObjectByValues = <T extends object, D extends T[keyof T][]>(o: T, forbiddenValues: D): T => {
+  const entries: EntryOf<T>[] = Object.entries(o).filter((entry: EntryOf<T>) => !forbiddenValues.includes(entry[1]));
   return Object.fromEntries(entries);
 };
