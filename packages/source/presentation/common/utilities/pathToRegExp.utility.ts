d30669eaa65bb5a79e97f903a63759f9a6c558e0
Semakov Andrey <semakov.andrey.web@yandex.ru>
2024-01-18 03:36:17 +0600

    c

--- packages/source/presentation/common/utilities/pathToRegExp.utility.ts
+++ packages/source/presentation/common/utilities/pathToRegExp.utility.ts
@@ -8,7 +8,7 @@ export interface PathToRegexpReturn {
 }
 
 export const pathToRegexp = (path: string): PathToRegexpReturn => {
-  const groupRx = /\/:([A-Za-z0-9_]+)(\([A-Za-z0-9|]+\))?(\?)?(\+)?/gu;
+  const groupRx = /\/:([A-Za-z0-9_]+)(\([A-Za-z0-9|]+\))?(\?)?/gu;
 
   let match: RegExpExecArray | null = groupRx.exec(path);
   let lastIndex = 0;
@@ -16,7 +16,7 @@ export const pathToRegexp = (path: string): PathToRegexpReturn => {
   let result = '';
 
   while (match !== null) {
-    const [ , segment, choices, optional, rest ] = match;
+    const [ , segment, choices, optional ] = match;
 
     const prefix = path[match.index - 1] === '/' ? 1 : 0;
     const prev = path.substring(lastIndex, match.index - prefix);
@@ -25,12 +25,10 @@ export const pathToRegexp = (path: string): PathToRegexpReturn => {
     lastIndex = groupRx.lastIndex;
 
     const newRegExp = isset(choices)
-      ? `/([A-Za-z0-9_]+)${ choices }`
+      ? `/${ choices }`
       : isset(optional)
         ? '/?([A-Za-z0-9_]+)?'
-        : isset(rest)
-          ? '/([A-Za-z0-9_\\-\\/\\.]+)'
-          : '/([A-Za-z0-9_]+)';
+        : '/([A-Za-z0-9_]+)';
     result += `${ escapeRx(prev) }${ newRegExp }`;
     match = groupRx.exec(path);
   }
