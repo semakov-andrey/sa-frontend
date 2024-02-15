2695d196427fd498bfd0f474948aaf65083aa35d
Semakov Andrey <semakov.andrey.web@yandex.ru>
2024-02-16 01:37:51 +0600

    c

--- packages/tools/linting/eslint/configs/fc-sorting.config.cjs
+++ packages/tools/linting/eslint/configs/fc-sorting.config.cjs
@@ -4,6 +4,7 @@ module.exports = {
       'error',
       { groups: [
         'useInject',
+        'useSyncExternalStore',
         'useState',
         'useRef',
         'useEffect',
--- packages/tools/linting/eslint/rules/fc-sorting.rule.cjs
+++ packages/tools/linting/eslint/rules/fc-sorting.rule.cjs
@@ -1,4 +1,5 @@
 const DEFAULT_GROUPS = [
+  'useSyncExternalStore',
   'useState',
   'useRef',
   'useEffect',
