4e35a7e22f61bd5c31624a602715e9b18b8d954a
Semakov Andrey <semakov.andrey.web@yandex.ru>
2024-02-18 05:04:23 +0600

    c

--- packages/source/presentation/common/hooks/useAsyncInfluence.hook.ts
+++ packages/source/presentation/common/hooks/useAsyncInfluence.hook.ts
@@ -2,8 +2,15 @@ import { useInfluence } from './useInfluence.hook';
 
 export type IsMounted = () => boolean;
 export type Setup = (isMounted: IsMounted) => Promise<void>;
+export type Options = { skip?: boolean };
 
-export const useAsyncInfluence = (func: Setup, deps: unknown[]): void => {
+export const useAsyncInfluence = (
+  func: Setup,
+  deps: unknown[],
+  opts: Options = {}
+): void => {
+  const { skip = false } = opts;
+
   useInfluence(() => {
     let isMounted = true;
     func(() => isMounted);
@@ -11,5 +18,5 @@ export const useAsyncInfluence = (func: Setup, deps: unknown[]): void => {
     return () => {
       isMounted = false;
     };
-  }, deps);
+  }, deps, { skip });
 };
--- packages/source/presentation/common/hooks/useInfluence.hook.ts
+++ packages/source/presentation/common/hooks/useInfluence.hook.ts
@@ -1,1 +1,20 @@
-export { useEffect as useInfluence } from 'react';
+import { useEffect } from 'react';
+
+export type IsMounted = () => boolean;
+export type Setup = () => void | (() => void);
+export type Options = { skip?: boolean };
+
+export const useInfluence = (
+  func: Setup,
+  deps: unknown[],
+  opts: Options = {}
+): void => {
+  const { skip = false } = opts;
+
+  useEffect(() => {
+    if (skip) return;
+    const destroy = func();
+
+    return destroy;
+  }, [ ...deps, skip ]);
+};
