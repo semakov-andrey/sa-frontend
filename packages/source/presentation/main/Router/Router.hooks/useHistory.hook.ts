43aa453701eaa1aa95c5ec64abe9335cf8ed5930
Semakov Andrey <semakov.andrey.web@yandex.ru>
2024-02-19 03:06:05 +0600

    c

--- packages/source/presentation/main/Router/Router.hooks/useHistory.hook.ts
+++ packages/source/presentation/main/Router/Router.hooks/useHistory.hook.ts
@@ -1,10 +1,12 @@
+import { useMemo } from 'react';
+
 export interface UseHistoryReturn {
   push: (path: string) => void;
   replace: (path: string) => void;
   back: () => void;
 }
 
-export const useHistory = (): UseHistoryReturn => ({
+export const useHistory = (): UseHistoryReturn => useMemo(() => ({
   push: (path: string): void => {
     window.dispatchEvent(new CustomEvent('history-push', { detail: path }));
   },
@@ -14,4 +16,4 @@ export const useHistory = (): UseHistoryReturn => ({
   back: (): void => {
     window.dispatchEvent(new CustomEvent('history-back', { detail: 'back' }));
   }
-});
+}), []);
