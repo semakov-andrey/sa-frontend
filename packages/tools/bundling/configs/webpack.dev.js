e839a7ea757cff4cbcf90a640dffd2b1609d715a
Semakov Andrey <semakov.andrey.web@yandex.ru>
2024-05-19 02:59:27 +0400

    c

--- packages/tools/bundling/configs/webpack.dev.js
+++ packages/tools/bundling/configs/webpack.dev.js
@@ -36,7 +36,7 @@ export const webpackDevConfig = (config, params) => {
     mode: 'development',
     ...isSourceMap ? { devtool: 'source-map' } : {},
     entry: [
-      'webpack-hot-middleware/client?reload=true',
+      ...isHMR ? [ 'webpack-hot-middleware/client?reload=true' ] : [],
       path.resolve(sourceDirectory, 'index.ts')
     ],
     output: {
